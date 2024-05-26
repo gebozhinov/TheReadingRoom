f8b98cbfa894b9946b7c7608bd2187b3be5208c6
Georgi Bozhinov <ge.bozhinov@gmail.com>
2024-05-26 18:34:01 +0300

    add sample test

--- src/test/java/com/thereadingroom/integration/web/BookControllerIntegrationTest.java
+++ src/test/java/com/thereadingroom/integration/web/BookControllerIntegrationTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -60,5 +61,28 @@ public class BookControllerIntegrationTest extends IntegrationTestInit {
         List<BookEntity> all = bookRepository.findAll();
 
         assertThat(all.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void testAddBook2() throws Exception {
+        BookEntity expected = new BookEntity();
+        expected.setAuthor("Kent Beck");
+        expected.setTitle("Test-Driven Development By Example");
+        expected.setDescription("Clean code that works");
+        expected.setPrice(BigDecimal.valueOf(10.99));
+        expected.setStock(10);
+
+        mockMvc.perform(post("/api/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(expected)))
+                .andExpect(status().isCreated());
+
+        BookEntity actual = bookRepository.findById(1L).get();
+
+        assertThat(actual.getAuthor()).isEqualTo(expected.getAuthor());
+        assertThat(actual.getTitle()).isEqualTo(expected.getTitle());
+        assertThat(actual.getDescription()).isEqualTo(expected.getDescription());
+        assertThat(actual.getPrice()).isEqualTo(expected.getPrice());
+        assertThat(actual.getStock()).isEqualTo(expected.getStock());
     }
 }
